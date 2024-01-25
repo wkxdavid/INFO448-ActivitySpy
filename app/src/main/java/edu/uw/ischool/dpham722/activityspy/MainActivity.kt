c56f37ef28ecaec176c2b47712cb51c584615a20
wkxdavid <dpham722@uw.edu>
2024-01-25 13:04:38 -0800

    finished homework + screenshots 2?

--- app/src/main/java/edu/uw/ischool/dpham722/activityspy/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/dpham722/activityspy/MainActivity.kt
@@ -2,10 +2,42 @@ package edu.uw.ischool.dpham722.activityspy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart event fired")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume event fired")
+    }
+
+    override fun onPause() {
+        super.onPause(  )
+        Log.i(TAG, "onPause event fired")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop event fired")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy event fired")
+        Log.e(TAG, "We're going down, Captain!")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart event fired")
     }
 }
